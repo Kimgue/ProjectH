90b1ed94dd50604074a26a06d2350b09145e6acc
khs <sososogoa@naver.com>
2022-02-20 19:54:55 +0900

    메뉴코드 수정

--- src/main/webapp/resources/js/gifticon/gifticonInsert.js
+++ src/main/webapp/resources/js/gifticon/gifticonInsert.js
@@ -68,14 +68,14 @@ var menuSltBool = false;
 			case 'H1':
 				$("#menuCode option").remove();
 				$("#menuCode").append("<option selected disabled>메뉴를 선택해주세요</option>");
-				$("#menuCode").append("<option value=01>빅맥</option>");
-				$("#menuCode").append("<option value=02>1955버거</option>");
+				$("#menuCode").append("<option value=1>빅맥</option>");
+				$("#menuCode").append("<option value=2>1955버거</option>");
 				break;
 			case 'H2':
 				$("#menuCode option").remove();
 				$("#menuCode").append("<option selected disabled>메뉴를 선택해주세요</option>");
-				$("#menuCode").append("<option value=01>새우버거</option>");
-				$("#menuCode").append("<option value=02>불고기버거</option>");
+				$("#menuCode").append("<option value=1>새우버거</option>");
+				$("#menuCode").append("<option value=2>불고기버거</option>");
 				break;
 			}
 		}
