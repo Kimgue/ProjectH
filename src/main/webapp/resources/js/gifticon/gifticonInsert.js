ff9dcdcd7f95493579b1050e8fcd97cf8701741a
khs <sososogoa@naver.com>
2022-02-20 20:06:02 +0900

    k

--- src/main/webapp/resources/js/gifticon/gifticonInsert.js
+++ src/main/webapp/resources/js/gifticon/gifticonInsert.js
@@ -65,13 +65,13 @@ var menuSltBool = false;
 			var brandCode = $("#brandCode").val();
 			
 			switch(brandCode) {
-			case 'H1':
+			case '1':
 				$("#menuCode option").remove();
 				$("#menuCode").append("<option selected disabled>메뉴를 선택해주세요</option>");
 				$("#menuCode").append("<option value=1>빅맥</option>");
 				$("#menuCode").append("<option value=2>1955버거</option>");
 				break;
-			case 'H2':
+			case '2':
 				$("#menuCode option").remove();
 				$("#menuCode").append("<option selected disabled>메뉴를 선택해주세요</option>");
 				$("#menuCode").append("<option value=1>새우버거</option>");
