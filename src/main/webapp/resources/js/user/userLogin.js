a1d4b7c07a56bf471ab66ae9f8606b9fe0c60abf
khs <sososogoa@naver.com>
2022-02-23 21:04:00 +0900

    관리자 로그인

--- src/main/webapp/resources/js/user/userLogin.js
+++ src/main/webapp/resources/js/user/userLogin.js
@@ -2,10 +2,13 @@
 		var idChk = $("#Id").val();
 		var pwChk = $("#Pw").val();
 
-		if (idChk == "HungPick1" && pwChk == "12341234") {
+		if (idChk == "admin1" && pwChk == "12341234") {
 			$("#login").attr("action", "adminLoginTry");
 			$("#login").submit();
 			
+		} else if(idChk == "admin2" && pwChk == "12341234") {
+			$("#login").attr("action", "adminLoginTry");			
+			$("#login").submit();			
 		} else {
 			$("#login").attr("action", "userLoginTry");
 			$("#login").submit();
